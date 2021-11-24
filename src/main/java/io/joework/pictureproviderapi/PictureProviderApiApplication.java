da087d6e2eb3065b2af5990bb96f15e11a0c9615
= <yousfessaam2323@gmail.com>
2021-11-24 07:54:24 +0200

    inital commit

---
+++ src/main/java/io/joework/pictureproviderapi/PictureProviderApiApplication.java
@@ -0,0 +1,13 @@
+package io.joework.pictureproviderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureProviderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureProviderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/joework/pictureproviderapi/PictureProviderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joework.pictureproviderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureProviderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
